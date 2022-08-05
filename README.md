ccf1fa0a0665653804d4611ae8cc0b07afd35fc2
ikenley <ikenley6@gmail.com>
2022-08-05 15:21:59 -0400

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# terraform-interview
+
+This is a small Terraform project intended for code interviews.
+
+Review this Terraform project and suggest any security, syntax, or design refactoring. Feel free to consult any [documenation](https://registry.terraform.io/providers/hashicorp/aws/latest/docs), [repositories](https://registry.terraform.io/), or other sources of inspiration.
---
+++ main.tf
@@ -0,0 +1,39 @@
+# ------------------------------------------------------------------------------
+# Create three RDS instances
+# ------------------------------------------------------------------------------
+
+terraform {
+  required_version = ">= 0.14"
+
+  backend "s3" {
+    profile = "terraform-dev"
+    region  = "us-east-1"
+    bucket  = "924586450630-terraform-state"
+    key     = "terraform-interview/terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "terraform-dev"
+}
+
+resource "aws_db_subnet_group" "this" {
+  name = "demo-subnet-group"
+
+  subnet_ids = ["subnet-096820ec1fa83d3c0", "subnet-0dbb11e63ec5c7dfa"]
+}
+
+resource "aws_db_instance" "foo" {
+  identifier           = "foo"
+  allocated_storage    = 10
+  engine               = "postgres"
+  engine_version       = "14.2"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "user_foo"
+  password             = "password_foo"
+  parameter_group_name = "default.postgres14"
+  skip_final_snapshot  = true
+  db_subnet_group_name = aws_db_subnet_group.this.id
+}
